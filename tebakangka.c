fbe7c22a686362b830201b6b308bf572a050c2a9
Timothy Marvine <timothymarvine@gmail.com>
2024-11-20 19:26:27 +0700

    fix: tebakangka.c pada yes or no

--- tebakangka.c
+++ tebakangka.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "boolean.h"
 
 int generateRandomNumber(int min, int max) {
     time_t t;
@@ -20,20 +21,21 @@ void W0RDL399(){
 }
 
 void tebakAngka(int *uang_asal){
-    int biaya_main = 200, tebakAngka, RNGnumber, hadiah = 500, kesempatan = 10, hadiah_sisa_uang;
+    int biaya_main = 200, tebakAngka, RNGnumber, hadiah = 500, hadiah_sisa_uang;
+    int kesempatan = 10;
+    boolean tebakanBenar = false;
     if (*uang_asal < biaya_main){
         printf("Uang Anda tidak cukup!");
     }
 
     *uang_asal -= biaya_main;
-    printf("Uang Anda tersisa %d.\n", *uang_asal);
+    printf("Uang Anda tersisa %d.\n\n", *uang_asal);
 
-    printf("Tebak Angka (1-100) dengan kesempatan tersisa %d \n", kesempatan);
+    printf("Tebak Angka (1-100) dengan kesempatan tersisa %d.\n", kesempatan);
 
     RNGnumber = generateRandomNumber(1,100);
-    printf("%d",RNGnumber);
 
-    while(kesempatan > 0){
+    while(kesempatan > 0 && tebakanBenar == false){
         printf("Tebak Angka = ");
         scanf("%d", &tebakAngka);
         if (tebakAngka < RNGnumber){
@@ -41,24 +43,28 @@ void tebakAngka(int *uang_asal){
         }else if(tebakAngka > RNGnumber){
             printf("Tebakanmu lebih besar!\n");
         }else if(tebakAngka == RNGnumber){
-            
+            tebakanBenar = true;
             if(kesempatan == 10){
                 hadiah_sisa_uang = 550;
                 printf("Tebakanmu benar! %d rupiah telah ditambahkan ke akun Anda\n",hadiah_sisa_uang);
+                *uang_asal += hadiah_sisa_uang;
                 printf("Uang anda sekarang adalah = %d.\n", *uang_asal);
                 return;
             }else{
                 hadiah_sisa_uang = 550 - 50*(10-kesempatan);
                 printf("Tebakanmu benar! %d rupiah telah ditambahkan ke akun Anda\n",hadiah_sisa_uang);
+                *uang_asal += hadiah_sisa_uang;
                 printf("Uang anda sekarang adalah = %d.\n", *uang_asal);
                 return;
             }
-            }
+
+        }
     kesempatan--;
     printf("Sisa kesempatan adalah %d.\n\n", kesempatan);
     }
     hadiah_sisa_uang = 550 - 50*(10-kesempatan);
     printf("Kesempatan Anda sudah habis. Angka yang benar adalah %d. Uang Anda tersisa %d\n", RNGnumber, hadiah_sisa_uang);
+
 }
 
 void workChallenge(int *uang_asal){
@@ -68,6 +74,7 @@ void workChallenge(int *uang_asal){
     printf("2. W0RDL399 (biaya main=500)\n");
     printf("Masukkan challenge yang hendak dimainkan: ");
     scanf("%d", &pilihanchallenge);
+    printf("\n");
 
     switch (pilihanchallenge){
         case 1:
@@ -82,23 +89,23 @@ void workChallenge(int *uang_asal){
     }
 }
 
-void yesorno(int *uang_asal){
-    char pilihanyesorno;
-    printf("Apakah anda masih mau bermain? (Y/N)\n");
-    scanf(" %c", &pilihanyesorno);
-
-    switch (pilihanyesorno){
-        case 'Y':
-            workChallenge(uang_asal);
-            return;
-        case 'N':
-            break;
-    }
-}
-
 int main(){
     int uang_asal = 5000;
+    char kesempatan;
+    boolean tebakanBenar;
+
     workChallenge(&uang_asal);
-    yesorno(&uang_asal);
+
+    char pilihanyesorno = 'Y';
+
+    while (pilihanyesorno != 'N'){
+        printf("Apakah anda masih mau bermain? (Y/N)\n");
+        scanf(" %c", &pilihanyesorno);
+        if (pilihanyesorno == 'Y'){
+            workChallenge(&uang_asal);
+        }else{
+            printf("Terima kasih sudah bermain Tebak Angka!");
+        }
+    }
     return 0;
 }
