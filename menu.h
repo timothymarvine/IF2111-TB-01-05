e494881a07544d72f25565ed61420d5921016961
Timothy Marvine <timothymarvine@gmail.com>
2024-11-22 19:24:00 +0700

    add: penambahan file header menu.h

---
+++ menu.h
@@ -0,0 +1,18 @@
+#ifndef MENU_H
+#define MENU_H
+
+int manual_strcmp(char *str1, char *str2);
+
+void WelcomeMenu();
+
+void LoginMenu();
+
+void helpinwelcome();
+
+void helpinlogin();
+
+void helpinmain();
+
+void MainMenu();
+
+#endif
