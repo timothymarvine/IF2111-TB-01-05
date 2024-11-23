bb97d45dde381fe4babb070425cdb5bd75eae10f
Timothy Marvine <timothymarvine@gmail.com>
2024-11-23 14:36:15 +0700

    fix mainmenu.c

--- mainmenu.c
+++ mainmenu.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include "menu.h"
 #include "workChallenge.h"
+#include "work.h"
+#include "mesinkata.h"
+#include "mesinkarakter.h"
+
 int main() {
+    char convertwsmm[50]; // Ubah menjadi array karakter untuk menampung string
     char command[10];
     int isLoggedIn = 0; // Status login pengguna (0 = tidak login, 1 = login)
     int running = 1;    // Status jalannya program (0 = berhenti, 1 = berjalan)
@@ -12,14 +17,25 @@ int main() {
         if (!isLoggedIn) {
             // Menampilkan Welcome Menu
             WelcomeMenu();
-            scanf("%s", command);
+            STARTWORD();
+            WordToString(currentWord, convertwsmm); // Menggunakan array char untuk string
+
+            for (int i = 0; i < currentWord.Length; i++) {
+                command[i] = convertwsmm[i];
+            }
+            command[currentWord.Length] = '\0';
 
             if (manual_strcmp(command, "START") == 0) {
                 // Masuk ke Login Menu
                 int loginMenuActive = 1;
                 while (loginMenuActive) {
                     LoginMenu();
-                    scanf("%s", command);
+                    STARTWORD();
+                    WordToString(currentWord, convertwsmm); // Menggunakan array char untuk string
+                    for (int i = 0; i < currentWord.Length; i++) {
+                        command[i] = convertwsmm[i];
+                    }
+                    command[currentWord.Length] = '\0';
                     printf("\n");
 
                     if (manual_strcmp(command, "LOGIN") == 0) {
@@ -50,17 +66,25 @@ int main() {
         } else {
             // Menampilkan Main Menu setelah Login
             MainMenu();
-            scanf("%s", command);
+            STARTWORD();
+            WordToString(currentWord, convertwsmm); // Menggunakan array char untuk string
+
+            for (int i = 0; i < currentWord.Length; i++) {
+                command[i] = convertwsmm[i];
+            }
+            command[currentWord.Length] = '\0';
             printf("\n");
 
-            if(manual_strcmp(command, "HELP") == 0){
+            if (manual_strcmp(command, "HELP") == 0) {
                 helpinmain();
-            }else if (manual_strcmp(command, "QUIT") == 0) {
+            } else if (manual_strcmp(command, "QUIT") == 0) {
                 printf("\nExiting program...\n");
                 running = 0;
-            }else if (manual_strcmp(command, "WORKCHALLENGE") == 0){
+            } else if (manual_strcmp(command, "WORK") == 0) {
+                work(&uang_asal);
+            } else if (manual_strcmp(command, "WORK CHALLENGE") == 0) {
                 workChallenge(&uang_asal);
-            }else {
+            } else {
                 printf("\nInvalid command. Please try again.\n");
             }
         }
