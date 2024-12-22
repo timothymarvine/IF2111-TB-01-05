15e3f206fac739ea422393e33fb9cf92591661a1
Ano <163797569+Naufalano@users.noreply.github.com>
2024-12-22 20:58:11 +0700

    Delete src/start.c

--- src/start.c
+++
@@ -1,7 +0,0 @@
-#include "start.h"
-#include "load.h"
-#include <stdio.h>
-
-void ConfigReader(int *itemCtr, int *userCtr, User sys[], Barang list[]){
-    LOAD("config.txt", list, *itemCtr, sys, userCtr);
-}
