1f627bbf9d277b8492c8e9a4ee47bd214c4e4379
Ano <163797569+Naufalano@users.noreply.github.com>
2024-12-22 21:06:55 +0700

    fixed .bat bug

--- bin/PURRMART.bat
+++ bin/PURRMART.bat
@@ -1,2 +1,2 @@
-gcc -o PURRMART ..\src\ADT\array\arraydin.c ..\src\ADT\mesinkarakter\mesinkarakter.c ..\src\ADT\mesinkata\mesinkata.c ..\src\ADT\queue\queue.c src\ADT\map\map.c ..\src\ADT\stack\stack.c ..\src\ADT\linkedlist\linkedlist.c ..\src\main2.c ..\src\console.c ..\src\menu.c
+gcc -o PURRMART ..\src\ADT\array\arraydin.c ..\src\ADT\mesinkarakter\mesinkarakter.c ..\src\ADT\mesinkata\mesinkata.c ..\src\ADT\queue\queue.c ..\src\ADT\map\map.c ..\src\ADT\stack\stack.c ..\src\ADT\linkedlist\linkedlist.c ..\src\main2.c ..\src\console.c ..\src\menu.c
 ./PURRMART
